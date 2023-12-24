29031a20f04a17cdc865b1bf69e6cfdbc0b8c312
LAPTOP-0HPUCQM3\Yunyu <yunyuancheng@outlook.com>
2023-12-24 12:42:40 +0800

    complete loader when getting message

--- QuoteGenerator/index.js
+++ QuoteGenerator/index.js
@@ -1,23 +1,36 @@
 let quotes = [];
 
 async function newQuote() {
+  loading();
   let quote = "";
   quote = quotes[Math.floor(Math.random() * quotes.length)];
   $(".quote-text").text(quote["text"]);
   $(".quote-author").text(quote["author"]);
+  complete();
 }
 
 async function getQuotes() {
+  loading();
   const apiURL = "https://type.fit/api/quotes";
   try {
     let quote = "";
     const resp = await fetch(apiURL);
     quotes = await resp.json();
     quote = quotes[Math.floor(Math.random() * quotes.length)];
-    console.log(quote);
     $(".quote-text").text(quote["text"]);
     $(".quote-author").text(quote["author"]);
+    complete();
   } catch (error) {}
 }
-$("#new-quote-btn").on("click", newQuote);
+
+function loading() {
+  $(".quote-container").hide();
+  $(".loader").show();
+}
+function complete() {
+  $(".quote-container").show();
+  $(".loader").hide();
+}
+
 getQuotes();
+$("#new-quote-btn").on("click", newQuote);
