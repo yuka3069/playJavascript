d1c083bac6c6c599c71651fd2e11cfe9c47bbed5
LAPTOP-0HPUCQM3\Yunyu <yunyuancheng@outlook.com>
2023-12-26 13:02:49 +0800

    complte fetching data and displaying photos

--- Infinite-scroll/readdoc.md
+++ Infinite-scroll/readdoc.md
@@ -21,3 +21,24 @@ width: 100%;
 .image-container img：这是一个 CSS 选择器，用于选择 ".image-container" 元素内部的图像（<img>）元素。
 
 width: 100%;：这行代码将选定的图像元素的宽度设置为父元素（即 ".image-container"）的宽度的 100%。这意味着图像将填充其容器的宽度，以适应容器的大小，而不会变形。
+
+const item = $("<a></a>");
+item.attr("href", photo.links.html);
+item.attr("target", "\_blank");
+
+item.append(img);
+$("#image-container").append(item);
+有关使用 jQuery 添加标签，更改 attr
+
+window.innerHeight：
+
+window.innerHeight 是浏览器窗口的视口高度，即浏览器窗口的可见部分的高度（以像素为单位）。它表示你当前能够在浏览器中看到的内容的垂直高度。
+这个值在响应式网页设计中非常有用，因为它可以帮助你确定浏览器窗口的可视区域大小，从而根据不同的窗口高度来调整网页的布局。
+window.scrollY：
+
+window.scrollY 是浏览器窗口在垂直方向上已滚动的距离（以像素为单位）。它表示你当前滚动条的位置距离页面顶部的垂直偏移量。
+这个值可用于检测用户滚动页面的行为，从而执行特定的 JavaScript 操作，例如在页面滚动时加载更多内容或执行吸顶效果等。
+document.body.offsetHeight：
+
+document.body.offsetHeight 是整个页面内容的高度，包括页面视口之外的部分。它表示整个页面的垂直高度（以像素为单位）。
+这个值通常用于确保页面的内容适应页面视口，以便在滚动时能够触发加载更多内容或执行一些其他滚动相关的操作。
--- Infinite-scroll/script.js
+++ Infinite-scroll/script.js
@@ -1,23 +1,50 @@
-const count = 10;
+const count = 30;
 const apiKey = "-AychDWElHhEzu0KQFYoxNTEqvaXTd0DT3SaK-oEfrM";
-const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 let photoArray = [];
 //get photo from api
 
+function imageLoaded() {
+  console.log("image loaded");
+}
 async function getPhotos() {
   try {
+    $("#loader").show();
+    $("#image-container").hide();
+
     const resp = await fetch(apiUrl);
     photoArray = await resp.json();
-    console.log(photoArray);
+
+    $("#loader").hide();
+    $("#image-container").show();
     display();
   } catch (error) {}
 }
 async function display() {
   photoArray.forEach((photo) => {
-    $("#image-container").append("<p>");
+    const item = $("<a></a>");
+    item.attr("href", photo.links.html);
+    item.attr("target", "_blank");
+
+    const img = $("<img>");
+    img.attr("src", photo.urls.regular);
+    img.attr("alt", photo.alt_description);
+    img.attr("title", photo.alt_description);
+
+    // put img into item and put item into imagecontainer
+    item.append(img);
+    // check if image loaded
+    img.on("load", imageLoaded);
+    $("#image-container").append(item);
   });
-}
-
+} // need a helper function
+$(window).on("scroll", () => {
+  if (
+    $(window).innerHeight() + $(window).scrollTop() >=
+    $(document).height() - 1000
+  ) {
+    getPhotos();
+  }
+});
 //On load
-
 getPhotos();
